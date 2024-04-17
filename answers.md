8e1ecef881aa74d8396d874edbd1ef102e18792a
iratzan <126986318+iratzan@users.noreply.github.com>
2024-04-17 04:22:10 +0000

    Final Commit

--- answers.md
+++ answers.md
@@ -4,14 +4,28 @@
 **Name:** Isaac Ratzan
 
 
-1a) For a given amount N, choose the largest denomination (2^k) that fits within N each time until N goes down to 0.
+1a) For a given amount N, choose the largest denomination of (2^k) that fits within N each time and subtract N until it goes down to 0.
 
-1b) To find the optimal amount, find the highest denomination ≤ N iteratively. This greedy approach ensures the least amount of coins used. It is optimal because the solutions are solved through subproblem solutions, thus preserving optimal substructure properties where d > 2^k.
+1b) To find the optimal amount, find the highest denomination ≤ N iteratively where k is the highest integer such that 2^k <= N. This greedy approach ensures the least amount of coins used. It is optimal because the solutions are solved through a binary nature with subproblem solutions, thus preserving optimal substructure properties where d > 2^k.
 
-1c) Work = O(log (n)), Span = O(1).
+1c) Work = O(log (n)), Span = O(log(n)).
 
-2a) In a scenario where N=6 and there are denominations of {1, 4}, choosing 4 yields 2 coins, but it would be optimal to use 3 coins of denomination 1.
+2a) In a scenario where N=4 and there are denominations of {1,2, 4}. The greedy scheduler would choose to take 2 2s rather than choosing the 4. 
 
-2b) True because analyzing the denominations at each step allows the minimum number of coins used.
+2b) The optimal substructures means that the problem can be divided into the optimal solution to all of the subproblems This is true because analyzing the denominations at each step allows the minimum number of coins used. By adding one coin to the optimal solution, you can determine the optimal amount for N.
 
-2c) Work = O(nk) and Span = O(nk) with k being the number of denominations.
+2c) 
+change[d, n]:
+   k = len(d)
+   optimal[0] = 0
+   for i in range (1, n):
+      min = inf
+      for j in range (1,k):
+         if d[i] <= i:
+            if (1+optimal[i-d[j]) < min:
+               min = 1+optimal[i-d[j]
+               coin = j
+      optimal[i] = min
+return optimal
+
+Work = O(nk) and Span = O(N) with k being the number of denominations.
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import math, queue
 from collections import Counter
 
-####### Problem 3 #######
-
 test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
+alignments = [('b--ook', 'bac--k'), ('kook-ab-ur-ra', 'kooky-bi-rd--'), ('-ele-phant','relev--ant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
     # TO DO - modify to account for insertions, deletions and substitutions
@@ -16,44 +14,43 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:],T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
 def fast_MED(S, T, MED={}):
-    if (S, T) in MED:
+  if (S, T) in MED:
       return MED[(S, T)]
-    if not S:
+  if S == "":
       return len(T)
-    elif not T:
+  elif T == "":
       return len(S)
-    else:
-      if S[0] == T[0]:
-          MED[(S, T)] = fast_MED(S[1:], T[1:], MED)
-      else:
-          MED[(S, T)] = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED), fast_MED(S[1:], T, MED))
-      return MED[(S, T)]
+  elif S[0] == T[0]:
+      MED[(S, T)] = fast_MED(S[1:], T[1:], MED)
+  else:
+      MED[(S, T)] = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED))
+
+  return MED[(S, T)]
+
 
 def fast_align_MED(S, T, MED={}):
   if (S, T) in MED:
-    return MED[(S, T)]
-  if not S:
-    return len(T), "-" * len(T), T
-  elif not T:
-    return len(S), S, "-" * len(S)
+      return MED[(S, T)]
+  if S == "":
+      MED[(S, T)] = ("-" * len(T), T)
+      return MED[(S, T)]
+  if T == "":
+      MED[(S, T)] = (S, "-" * len(S))
+      return MED[(S, T)]
+  if S[0] == T[0]:
+      aligned_seq_S, aligned_seq_T = fast_align_MED(S[1:], T[1:], MED)
+      MED[(S, T)] = (S[0] + aligned_seq_S, T[0] + aligned_seq_T)
   else:
-    if S[0] == T[0]:
-        dist, S_aligned, T_aligned = fast_align_MED(S[1:], T[1:], MED)
-        MED[(S, T)] = dist, S[0] + S_aligned, T[0] + T_aligned
-    else:
-        ins_dist, ins_S_aligned, ins_T_aligned = fast_align_MED(S, T[1:], MED)
-        del_dist, del_S_aligned, del_T_aligned = fast_align_MED(S[1:], T, MED)
-        sub_dist, sub_S_aligned, sub_T_aligned = fast_align_MED(S[1:], T[1:], MED)
-        min_dist = min(ins_dist, del_dist, sub_dist)
-        if min_dist == ins_dist:
-            MED[(S, T)] = 1 + ins_dist, "-" + ins_S_aligned, T[0] + ins_T_aligned
-        elif min_dist == del_dist:
-            MED[(S, T)] = 1 + del_dist, S[0] + del_S_aligned, "-" + del_T_aligned
-        else:
-            MED[(S, T)] = 1 + sub_dist, S[0] + sub_S_aligned, T[0] + sub_T_aligned
-    return MED[(S, T)]
-
+      insert_seq_S, insert_seq_T = fast_align_MED(S, T[1:], MED)
+      delete_seq_S, delete_seq_T = fast_align_MED(S[1:], T, MED)
+      cost_insert = 1 + len(insert_seq_S)
+      cost_delete = 1 + len(delete_seq_S)
+      if cost_insert <= cost_delete:
+          MED[(S, T)] = ("-" + insert_seq_S, T[0] + insert_seq_T)
+      else:
+          MED[(S, T)] = (S[0] + delete_seq_S, "-" + delete_seq_T)
+  return MED[(S, T)]
